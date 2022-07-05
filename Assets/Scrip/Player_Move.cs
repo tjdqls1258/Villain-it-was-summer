03011a7c8b18c0c9cfd97728943e97c390ceb663
tjdqls1258 <42854743+tjdqls1258@users.noreply.github.com>
2022-07-06 01:51:41 +0900

    프로젝트 시작
    프로젝트 시작

---
+++ Assets/Scrip/Player_Move.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D), typeof(BoxCollider2D))]
+public class Player_Move : MonoBehaviour
+{
+    public float Move_Speed = 1.0f;
+    public float Jump_Power = 5.0f;
+
+    private Rigidbody2D rigid;
+    private void Awake()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        if(Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.position += Vector3.left * Move_Speed *Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.position += Vector3.right * Move_Speed * Time.deltaTime;
+        }
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            Jump();
+        }
+    }
+
+    private void Jump()
+    {
+        if(Mathf.Abs(rigid.velocity.y) > 0)
+        {
+            Debug.Log(rigid.velocity.y);
+            return;
+        }
+
+        rigid.AddForce(Vector2.up * Jump_Power , ForceMode2D.Impulse);
+    }
+}
