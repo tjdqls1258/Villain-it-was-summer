0528a33ad7643871e5e3153b4168616cd11b5a63
tjdqls1258 <42854743+tjdqls1258@users.noreply.github.com>
2022-07-15 00:36:58 +0900

    07-15 곽성빈 플레이어 대쉬, 에셋 추가
    에셋 추가
    플레이어 대쉬(애니메이션 없어서 이펙트로 대체)

--- Assets/Scrip/Player/Player_Move.cs
+++ Assets/Scrip/Player/Player_Move.cs
@@ -8,30 +8,47 @@ public class Player_Move : MonoBehaviour
     //public KeyCode Move_Left, Move_Right, Jump, ATK;
     public float Move_Speed = 1.0f;
     public float Jump_Power = 5.0f;
+    public float Dash_Power = 5.0f;
     public float ATK_Delay = 0.5f;
 
-    private bool Is_ATK = true;
+    private bool Is_ATK;
+    private bool Is_Dsah;
 
     private Rigidbody2D rigid;
     [SerializeField] private GameObject Down_Collider;
     [SerializeField] private GameObject ATK_Area;
+    [SerializeField] private GameObject Dash_Effect;
     public GameObject ATK_Ation;
     private void Awake()
     {
         rigid = GetComponent<Rigidbody2D>();
+        Is_ATK = true;
+        Is_Dsah = true;
     }
 
     private void Update()
     {
+        if(rigid.velocity.x != 0.0f)
+        {
+            return;
+        }
         if(Input.GetKey(KeyCode.LeftArrow))
         {
             transform.position += Vector3.left * Move_Speed *Time.deltaTime;
             ATK_Area.transform.position = transform.position + Vector3.left * 1.165f;
+            if (Input.GetKeyDown(KeyCode.X) && Is_Dsah)
+            {
+                Dash(Vector3.left);
+            }
         }
         if (Input.GetKey(KeyCode.RightArrow))
         {
             transform.position += Vector3.right * Move_Speed * Time.deltaTime;
             ATK_Area.transform.position = transform.position + Vector3.right * 1.165f;
+            if (Input.GetKeyDown(KeyCode.X) && Is_Dsah)
+            {
+                Dash(Vector3.right);
+            }
         }
 
         if(Input.GetKeyDown(KeyCode.Space))
@@ -52,6 +69,7 @@ public class Player_Move : MonoBehaviour
             ATK_Ation.SetActive(true);
             StartCoroutine(ATK());
         }
+
     }
 
     private void Jump()
@@ -64,11 +82,27 @@ public class Player_Move : MonoBehaviour
         rigid.AddForce(Vector2.up * Jump_Power , ForceMode2D.Impulse);
     }
 
+    private void Dash(Vector2 _Dash_Vector)
+    {
+        Is_Dsah = false;
+        rigid.AddForce(_Dash_Vector * Dash_Power, ForceMode2D.Impulse);
+        StartCoroutine(Dash_Timmer());
+    }
+    
     private IEnumerator ATK()
     {
         yield return new WaitForSeconds(0.2f);
         ATK_Ation.SetActive(false);
         yield return new WaitForSeconds(ATK_Delay - 0.2f);
         Is_ATK = true;
+    }
+
+    private IEnumerator Dash_Timmer()
+    {
+        Dash_Effect.SetActive(true);
+        yield return new WaitForSeconds(0.2f);
+        rigid.velocity = new Vector3(0.0f, rigid.velocity.y, 0.0f);
+        Dash_Effect.SetActive(false);
+        Is_Dsah = true;
     }
 }
