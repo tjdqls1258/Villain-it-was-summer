10b74da39c8639285682d7d63b6e652a399a4cee
tjdqls1258 <42854743+tjdqls1258@users.noreply.github.com>
2022-07-23 21:00:55 +0900

    07-23 곽성빈 스테이지 클리어 시 보상

---
+++ Assets/Scrip/Gate/Coverage.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coverage : MonoBehaviour
+{
+    public int Moeny = 100;
+    //public List<Item> Coverage_List = new List<Item>(); 
+    private SpriteRenderer sprite;
+
+    private void Awake()
+    {
+        sprite = GetComponent<SpriteRenderer>();
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.transform.tag =="Player")
+        {
+            sprite.enabled = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.transform.tag == "Player")
+        {
+            sprite.enabled = false;
+        }
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.transform.tag == "Player")
+        {
+            if (Input.GetKeyDown(KeyCode.X))
+            {
+
+                Debug.Log("보상을 얻음.");
+            }
+        }
+    }
+
+    public void coverage_To_Player(int Level)
+    {
+        GameObject player = GameObject.Find("Player");
+        Moeny *= Level;
+        //player.GetCompoent("Inven").Get_Item(Coverage_List[0,Coverage_List.count]);
+    }
+}
