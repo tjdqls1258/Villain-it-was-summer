2b582abe47ddbdab87ff7577906d2ea0b9f4e2ae
JiWonKimP <86395330+JiWonKimP@users.noreply.github.com>
2022-08-03 16:43:16 +0900

    타이틀 및 플레이 UI 초안 작업

---
+++ Assets/Scrip/TitleUI.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TitleUI : MonoBehaviour
+{
+    public void NewGame()
+    {
+        //처음부터 시작이므로 플레이 처음 씬으로
+        //SceneManager.LoadScene();
+    }
+
+    public void LoadGame()
+    {
+        //필요하게 되면 작성 - 현재는 더미
+        //SceneManager.LoadScene();
+    }
+
+    public void Tutorial()
+    {
+        //튜토리얼 씬으로 이동
+        //SceneManager.LoadScene();
+    }
+
+    public void ExitGame()
+    {
+        //게임종료
+        Application.Quit();
+    }
+}
